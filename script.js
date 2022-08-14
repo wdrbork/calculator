cf6e8f3f85d4fa2dcce044c2a2d0b2e028949041
wdrbork <wdrb2001@gmail.com>
2022-08-14 14:22:42 -0700

    Add number and operator buttons

--- script.js
+++ script.js
@@ -13,3 +13,15 @@ function multiply(x, y) {
 function divide(x, y) {
     return x / y;
 }
+
+function operate(operator, x, y) {
+    if (operator === '+') {
+        return add(x, y);
+    } else if (operator === '-') {
+        return subtract(x, y);
+    } else if (operator === '*') {
+        return multiply(x, y);
+    } else if (operator === '/') {
+        return divide(x, y);
+    }
+}
