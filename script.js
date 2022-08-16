577592447128e85c8651e1ce2976dfa6e47ed607
wdrbork <wdrb2001@gmail.com>
2022-08-15 17:52:03 -0700

    Allow user to do addition and subtraction
    Still contains some bugs relating to pressing "=". Otherwise, adding
    or subtracting multiple numbers together displays correct result.

--- script.js
+++ script.js
@@ -1,20 +1,62 @@
 let displayValue = 0;
-let operator = undefined;
+let currentSum = 0;
+let currentOperator = undefined;
 
 const display = document.querySelector('.display');
 const numbers = document.querySelectorAll('.number');
 numbers.forEach((number) => {
     number.addEventListener('click', () => {
-        displayValue = parseInt(number.textContent);
+        displayValue = displayValue * 10 + parseInt(number.textContent);
         display.textContent = displayValue;
+    });
+});
+
+const operators = document.querySelectorAll('.operator');
+operators.forEach((operator) => {
+    operator.addEventListener('click', () => {
+        if (currentOperator) {
+            if (operator.textContent === '+' || operator.textContent === '-' || 
+                    operator.textContent === '=') {
+                currentSum = operate(currentOperator, currentSum, displayValue);
+                display.textContent = currentSum;
+            }
+        } else {
+            currentSum = displayValue
+        }
+    
+        displayValue = 0;
+        if (operator.textContent === '=') {
+            currentOperator = undefined;
+        } else {
+            currentOperator = operator.textContent;
+        }
     });
 });
 
 const clear = document.querySelector('.clear');
 clear.addEventListener('click', () => {
     displayValue = 0;
+    currentSum = 0;
+    currentOperator = undefined;
     display.textContent = "";
 });
+
+function mathManager(operator) {
+    if (currentOperator) {
+        if (operator.textContent === '+' || operator.textContent === '-' || 
+                operator.textContent === '=') {
+            currentSum = operate(currentOperator, currentSum, displayValue);
+            display.textContent = currentSum;
+        }
+    }
+
+    displayValue = 0;
+    if (operator.textContent === '=') {
+        currentOperator = undefined;
+    } else {
+        currentOperator = operator.textContent;
+    }
+}
 
 function add(x, y) {
     return parseInt(x) + parseInt(y);
